fe37207ec1c9ed6eab472894964902275a96e908
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-06 12:48:07 -0600

    se termina archivo app

--- Proyecto Introducción a ECMAScript/app.js
+++ Proyecto Introducción a ECMAScript/app.js
@@ -12,5 +12,44 @@ const resumen = document.getElementById("resumen");
 const itinerario = document.getElementById("itinerario");
 
 //evento principal
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    //Obtener datos
+
+    const destino = destinoInput.value;
+    const fecha = fechaInput.value;
+    const transporte = transporteInput.value;
+    const personas = parseInt(personasInput.value);
+
+    //guardar viaje
+    registrarDestino(destino, fecha, transporte, personas);
+
+    //Calcular costo
+    const costo = calcularCosto(transporte, personas);
+
+    //Obtener sugerencia
+    const sugerencia = sugerenciaViaje(destino);
+
+    // Mostrar resumen
+    resumen.innerHTML = `
+    <h2>Resumen del viaje</h2>
+    <p><strong>Destino:</strong> ${destino}</p>
+    <p><strong>Total:</strong> $${costo}</p>
+    <p>${sugerencia}</p>
+    `;
+    //Mostrar itinerario
+    itinerario.innerHTML =
+    <h2>Itinerario</h2>
+    ${mostrarItinerario(viajes)}
+    ;
+
+    //Limpiar formulario
+    form.requestFullscreen();
+});
+
+
+
+
     
 }
