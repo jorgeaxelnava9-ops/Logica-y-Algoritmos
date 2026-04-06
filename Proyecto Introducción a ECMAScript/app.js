eaa1a17adc50716c64c907e2586d0effbcf09755
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-06 12:33:38 -0600

    se agrega archivo app

---
+++ Proyecto Introducción a ECMAScript/app.js
@@ -0,0 +1,16 @@
+import { viajes, registrarDestino, mostrarItinerario } from "./viajes";
+import { calcularCosto } from "./costos";
+
+//Elementos del DOM
+const form = document.getElementById("form-viaje");
+const destinoInput = document.getElementById("destino");
+const fechaInput = document.getElementById("fecha");
+const transporteInput = document.getElementById("transporte");
+const personasInput = document.getElementById("personas");
+
+const resumen = document.getElementById("resumen");
+const itinerario = document.getElementById("itinerario");
+
+//evento principal
+    
+}
