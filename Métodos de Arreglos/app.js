112b15e642d0a27df2fa044fd994febda5568526
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-07 10:55:00 -0600

    se comienza archivo js

---
+++ Métodos de Arreglos/app.js
@@ -0,0 +1,11 @@
+// 1. Arreglo de productos
+const productos = [
+    {nombre: "Audífonos", precio: 80, categoria: "Electrónica" },
+    {nombre: "Teclado", precio: 150, categoria: "Electrónica" },
+    {nombre: "Mouse", precio: 50, categoria: "Electrónica" },
+    {nombre: "Playera", precio: 90, categoria: "Ropa" },
+    {nombre: "Zapatos", precio: 200, categoria: "Ropa" },
+    {nombre: "Libro", precio: 70, categoria: "Educación" },
+];
+
+//Mostrar productos en pantalla
