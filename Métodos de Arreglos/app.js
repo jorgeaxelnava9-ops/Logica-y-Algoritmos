04015b41a97f424c7da21d6b9826e09a45c75667
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-07 11:05:22 -0600

    se agregan condiciones

--- Métodos de Arreglos/app.js
+++ Métodos de Arreglos/app.js
@@ -9,3 +9,36 @@ const productos = [
 ];
 
 //Mostrar productos en pantalla
+const contenedor = document.getElementById("productos");
+
+function mostrarProductos(lista) {
+    contenedor.innerHTML = "";
+
+    lista.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML =
+        <h3>${producto.nombre}</h3>
+        <p>💲 ${producto.precio}</p>
+        <p>📦 ${producto.categoria}</p>
+        ;
+
+        contenedor.appendChild(card);
+        });
+}
+
+//Mostrar todo al inicio
+mostrarProductos(productos);
+
+// Botón
+document.getElementById("btnFiltrar").addEventListener("click", () => {
+
+    //2. FILTER productos menores a 100
+    const filtrados = productos.filter(p => p.precio <100);
+    console.log("Filtrados (<100):", filtrados);
+
+    //3. SORT ordenar por nombre
+})
+
+
