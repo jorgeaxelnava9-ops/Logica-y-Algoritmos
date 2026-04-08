20eef5e535aa0a1b7991ecdc6e601b82829fabeb
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-08 09:43:03 -0600

    Se comienza instrucciones js

---
+++ Proyecto del Recursion y backtracking/script.js
@@ -0,0 +1,21 @@
+const regalos = [
+    "Muñeca",
+    "Carro",
+    "Videojuego",
+    "Bicicleta",
+    "Laptop",
+    "Celular",
+    "Pelota",
+    "Libros",
+    "Audífonos",
+    "Juegos de Mesa",
+    "Tazas",
+    "Flores",
+    "Relojes",
+    "Perfumes",
+    "Gafas de Sol",
+    "Carteras",
+    "Drones",
+];
+
+const 
