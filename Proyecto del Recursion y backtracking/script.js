693a91a960a09e9df429affb9fe210691ce0d560
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-08 13:01:19 -0600

    se concluyen instrucciones js

--- Proyecto del Recursion y backtracking/script.js
+++ Proyecto del Recursion y backtracking/script.js
@@ -18,4 +18,58 @@ const regalos = [
     "Drones",
 ];
 
-const 
+const lista = document.getElementById("lista");
+const resultado = resultado.getElementById("resultado");
+
+//Mostrar lista 
+function mostrarLista() {
+    lista.innerHTML = "";
+
+    regalos.forEach(regalo => {
+        const div = document.createElement("div");
+        div.classList.add("item");
+        div.textContent = regalo;
+        lista.appendChild(div);
+    });
+}
+
+mostrarLista();
+
+
+//FUNCIÓN RECURSIVA
+function buscarRegalo(lista, objetivo, index = 0) {
+
+    // CASO BASE 1: Se encontró
+    if (lista[index] === objetivo) {
+        return `🎁 Encontrado en la posición ${index}`;
+    }
+
+    // CASO BASE 2: No encontrado
+    if (index >= lista.lenght) {
+        return "❌ El regalo no está en la lista";
+    }
+
+    //LLAMADA RECURSIVA
+    return buscarRegalo(lista, objetivo, index + 1);
+}
+
+//Evento botón
+document.getElementById("btnBuscar").addEventListener("click", () => {
+
+    const valor = document.getElementById("inputRegalo").ariaValueMax;
+
+    const mensaje = buscarRegalo(regalo, valor);
+
+    resultado.textContent = mensaje;
+    console.log(mensaje);
+
+    //resaltar en UI 
+    const items = document.querySelectorAll(".item");
+    items.forEach(item => item.classList.remove("highlight"));
+
+    items.forEach(item => {
+        if (item.textContent === valor) {
+            items.classList.add("highlight");
+        }
+    });
+});
