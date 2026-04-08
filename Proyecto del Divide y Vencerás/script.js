b84f642a660e9fc6d69ff590d2aa509798d2360f
Jorge Axel Nava <jorgeaxelnava9@gmail.com>
2026-04-08 13:57:18 -0600

    se corrigen errores en código

--- Proyecto del Divide y Vencerás/script.js
+++ Proyecto del Divide y Vencerás/script.js
@@ -14,4 +14,45 @@ function encontrarMaximo(arr) {
     const mitad = Math.floor(arr.length /2);
     const izquierda = arr.slice(0, mitad);
     const derecha = arr.slice(mitad);
+
+    // CONQUISTAR (recursión)
+    const maxIzq = encontrarMaximo(izquierda);
+    const maxDer = encontrarMaximo(derecha);
+
+    // COMBINAR
+    return maxIzq > maxDer ? maxIzq : maxDer;
 }
+
+// Evento Botón
+document.getElementById("btnCalcular").addEventListener("click", () => {
+
+    const valores = input.value
+    .split(",")
+    .map(num => parseInt(num.trim()))
+    .filter(num => !isNaN(num));
+
+    if (valores.length === 0) {
+        resultado.textContent = "❌ Ingresa números válidos";
+        return;
+    }
+
+    const maximo = encontrarMaximo(valores);
+
+    resultado.textContent = `🔝 Número máximo: ${maximo}`;
+    console.log("Máximo:", maximo);
+
+    // Mostrar visual
+    visual.innerHTML = "";
+
+    valores.forEach(num => {
+        const div = document.createElement("div");
+        div.classList.add("bloque");
+        div.textContent = num;
+
+        if (num === maximo) {
+            div.classList.add("max");
+        }
+
+        visual.appendChild(div);
+    });
+});
